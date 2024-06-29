1650e260675e6e89864a218c9f9736f7eacc60da
emisua <91371538+emisua@users.noreply.github.com>
2024-06-30 01:22:21 +0200

    change url

--- app/animals/[path]/page.tsx
+++ app/animals/[path]/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 const page = async ({ params }: { params: { path: string } }) => {
-  const apiBaseUrl =
-    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api'
+  const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 
   const { path } = params
-  const response = await fetch(`${apiBaseUrl}/animals/${path}`)
+  const response = await fetch(`${apiBaseUrl}/api/animals/${path}`)
   const data = await response.json()
   console.log(data)
 
--- app/animals/page.tsx
+++ app/animals/page.tsx
@@ -2,9 +2,8 @@ import { AnimalsGrid } from '@/components/animalGrid/AnimalsGrid'
 import React from 'react'
 
 const page = async () => {
-  const apiBaseUrl =
-    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api'
-  const res = await fetch(`${apiBaseUrl}/animals`)
+  const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+  const res = await fetch(`${apiBaseUrl}/api/animals`)
   const { animals } = await res?.json()
 
   if (!animals) return <p>Cargando...</p>
